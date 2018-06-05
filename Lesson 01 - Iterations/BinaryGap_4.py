d18c3588945a1d2875f8b0004f0138fed6c4dfbd
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-05 07:04:54 +0300

    Update BinaryGap_4.py

--- Lesson 01 - Iterations/BinaryGap_4.py
+++ Lesson 01 - Iterations/BinaryGap_4.py
@@ -5,4 +5,16 @@ def solution(N):
     # write your code in Python 3.6
     
     N_binary = "{0:b}".format(N)
+    max_gap = 0
+    current_gap = -1
+    
+    for i in range(1, len(N_binary)):
+        if (N_binary[i-1] == '1' and N_binary[i] == '0'):
+            current_gap = 1
+        elif (N_binary[i - 1] == '0' and N_binary[i] == '0'):
+            current_gap += 1
+        elif (N_binary[i - 1] == '0' and N_binary[i] == '1'):
+            max_gap = max(max_gap, current_gap)
+        
+    return max_gap
     
