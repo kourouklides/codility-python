f8df350b54a6a2e292bac6d9c8ed834414f5d01b
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-05 06:58:35 +0300

    Rename BinaryGap_1.py to BinaryGap_4.py

---
+++ Lesson 01 - Iterations/BinaryGap_4.py
@@ -0,0 +1,8 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N):
+    # write your code in Python 3.6
+    
+    N_binary = "{0:b}".format(N)
+    
