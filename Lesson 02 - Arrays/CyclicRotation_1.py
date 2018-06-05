7c7cb21458cf2844ea3bd218a9d69890f7b66a17
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-06 01:50:52 +0300

    Update CyclicRotation_1.py

--- Lesson 02 - Arrays/CyclicRotation_1.py
+++ Lesson 02 - Arrays/CyclicRotation_1.py
@@ -2,4 +2,12 @@
 # print("this is a debug message")
 
 def solution(A, K):
-
+    # write your code in Python 3.6
+    
+    B = A[:] 
+    n = len(B)
+    
+    for i in range(0, n):
+        B[(i + K) % n] = A[i]
+    
+    return B
