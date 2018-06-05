57884be4213d57c34dd6f60de7f8322e357c6944
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-05 06:02:48 +0300

    Update BinaryGap_1.py

--- Lesson 01 - Iterations/BinaryGap_1.py
+++ Lesson 01 - Iterations/BinaryGap_1.py
@@ -3,5 +3,21 @@
 
 def solution(N):
     # write your code in Python 3.6
-    pass
     
+    i = N
+    counter = 0
+    result = 0
+    found_one = False
+    
+    while (i > 0):
+        if ( (i & 1) == 1):
+            if (found_one == False):
+                found_one = True
+            else:
+                result = max(result, counter)
+            counter = 0
+        else:
+            counter += 1
+        i >>= 1
+        
+    return result
