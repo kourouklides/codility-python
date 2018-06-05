03e3c0dafb97d04f678acba055983c3fffba29d9
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-06 02:22:51 +0300

    Update CyclicRotation_2.py

--- Lesson 02 - Arrays/CyclicRotation_2.py
+++ Lesson 02 - Arrays/CyclicRotation_2.py
@@ -4,3 +4,14 @@
 def solution(A, K):
     # write your code in Python 3.6
     
+    N = len(A)
+    
+    if ((N==0) or (K==0) or (K==N)):
+        return A
+    
+    if (K > N):
+        i = K % N
+    else:
+        i = K
+    
+    return A[-i:] + A[:-i]
