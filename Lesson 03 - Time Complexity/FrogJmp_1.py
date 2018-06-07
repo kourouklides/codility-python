abcb13f38ae0579642e3e3e208eb917c4f8a3c9d
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-07 07:08:31 +0300

    Create FrogJmp_1.py

---
+++ Lesson 03 - Time Complexity/FrogJmp_1.py
@@ -0,0 +1,14 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+import math
+
+def solution(X, Y, D):
+    # write your code in Python 3.6
+    
+    num = Y - X
+    den = D
+    jumps = math.ceil (num/den)
+    
+    return jumps
+    
